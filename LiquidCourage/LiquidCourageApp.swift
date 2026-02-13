9430d7de8dda0f9050513f9d50fdd9aace0fe3fc
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-13 17:42:34 +0000

    Initial Commit

---
+++ LiquidCourage/LiquidCourageApp.swift
@@ -0,0 +1,18 @@
+//
+//  LiquidCourageApp.swift
+//  LiquidCourage
+//
+//  Created by app-kaihatsusha on 13/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LiquidCourageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
