b5a2b7c46867f7fec976a5f4bf053c832fe0e6ca
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-15 17:54:04 +0000

    Swipe to accept reject functionality added

--- LiquidCourage/Core/HomeView.swift
+++ LiquidCourage/Core/HomeView.swift
@@ -13,6 +13,11 @@ struct HomeView: View {
     @State private var filters: [String] = ["Everyone", "Trending"]
     @AppStorage("home_filter") private var selectedFilter = "Everyone"
     
+    @State private var allUsers: [User] = []
+    @State private var selectedIndex: Int = 0
+    @State private var currentSwipeOffset: CGFloat = 0
+    @State private var cardOffsets: [Int : Bool] = [:] // userid : direction right = true
+    
     var body: some View {
         ZStack {
             Color.appWhite
@@ -26,12 +31,120 @@ struct HomeView: View {
                         Divider(), alignment: .bottom
                     )
                 
-                CardView()
-                
-                Spacer()
+                ZStack {
+                    if !allUsers.isEmpty {
+                        ForEach(Array(allUsers.enumerated()), id: \.offset) { (index, user) in
+                            
+                            let isPrevious = (selectedIndex - 1) == index
+                            let isCurrent = selectedIndex == index
+                            let isNext = (selectedIndex + 1) == index
+                            
+                            if isPrevious || isCurrent || isNext{
+                                
+                                let offsetValue = cardOffsets[user.id]
+                                userProfileCell(user: user, index: index)
+                                    .zIndex(Double(allUsers.count - index))
+                                    .offset(x: offsetValue == nil ? 0 : offsetValue == true ? 900 : -900)
+                            }
+                        }
+                    } else {
+                        ProgressView()
+                    }
+                    
+                    overlaySwipingIndictors
+                        .zIndex(999)
+                }
+                .frame(maxHeight: .infinity)
+                .padding(4)
+                .animation(.smooth,value: cardOffsets)
             }
             .padding(8)
         }
+        .task {
+            await getData()
+        }
+        .toolbarVisibility(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        
+        guard allUsers.isEmpty else { return }
+        
+        do {
+            allUsers = try await DatabaseHelper().getUsers()
+        } catch {
+            
+        }
+    }
+    
+    private func userDidSelect(index: Int, isLike: Bool) {
+        let user = allUsers[index]
+        cardOffsets[user.id] = isLike
+        
+        selectedIndex += 1
+    }
+    
+    private func userProfileCell(user: User, index: Int) -> some View {
+        
+        CardView(
+            user: user,
+            onSuperLikePressed: nil,
+            onXMarkPressed: { userDidSelect(index: index, isLike: false) },
+            onCheckMarkPressed: { userDidSelect(index: index, isLike: true) },
+            onSendAComplimentPressed: nil,
+            onHideAndReportPressed: nil
+        )
+        .withDragGesture(
+            .horizontal,
+            minimumDistance: 50,
+            resets: true,
+            rotationMultiplier: 1.05,
+            scaleMultiplier: 0.5,
+            onChanged: { dragOffset in
+                currentSwipeOffset = dragOffset.width
+            },
+            onEnded: { dragOffset in
+                if dragOffset.width < -200 {
+                    userDidSelect(index: index, isLike: false)
+                } else if dragOffset.width > 200 {
+                    userDidSelect(index: index, isLike: true)
+                }
+                
+            }
+        )
+    }
+    
+    private var overlaySwipingIndictors: some View {
+        
+        ZStack {
+            Circle()
+                .fill(.appGrey.opacity(0.4))
+                .overlay {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+                .frame(width: 60, height: 60)
+                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1.0)
+                .offset(x: min(-currentSwipeOffset, 150))
+                .offset(x: -100)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Circle()
+                .fill(.appGrey.opacity(0.4))
+                .overlay {
+                    Image(systemName: "checkmark")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+                .frame(width: 60, height: 60)
+                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1.0)
+                .offset(x: max(-currentSwipeOffset, -150))
+                .offset(x: 100)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+        }
+        .animation(.smooth,value: currentSwipeOffset)
     }
     
     
@@ -51,7 +164,7 @@ struct HomeView: View {
                     }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-           
+            
             
             Text("LiquidCourage")
                 .font(.title)
---
+++ LiquidCourage/ViewModifiers/DragGesture.swift
@@ -0,0 +1,181 @@
+//
+//  DragGesture.swift
+//  LiquidCourage
+//
+//  Created by app-kaihatsusha on 15/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct DragGestureViewModifier: ViewModifier {
+    
+    @State private var offset: CGSize = .zero
+    @State private var lastOffset: CGSize = .zero
+    @State private var rotation: Double = 0
+    @State private var scale: CGFloat = 1
+
+    let axes: Axis.Set
+    let minimumDistance: CGFloat
+    let resets: Bool
+    let animation: Animation
+    let rotationMultiplier: CGFloat
+    let scaleMultiplier: CGFloat
+    let onChanged: ((_ dragOffset: CGSize) -> ())?
+    let onEnded: ((_ dragOffset: CGSize) -> ())?
+
+    init(
+        _ axes: Axis.Set = [.horizontal, .vertical],
+        minimumDistance: CGFloat = 0,
+        resets: Bool,
+        animation: Animation,
+        rotationMultiplier: CGFloat = 0,
+        scaleMultiplier: CGFloat = 0,
+        onChanged: ((_ dragOffset: CGSize) -> ())?,
+        onEnded: ((_ dragOffset: CGSize) -> ())?) {
+            self.axes = axes
+            self.minimumDistance = minimumDistance
+            self.resets = resets
+            self.animation = animation
+            self.rotationMultiplier = rotationMultiplier
+            self.scaleMultiplier = scaleMultiplier
+            self.onChanged = onChanged
+            self.onEnded = onEnded
+        }
+        
+    func body(content: Content) -> some View {
+        content 
+            .scaleEffect(scale)
+            .rotationEffect(Angle(degrees: rotation), anchor: .center)
+            .offset(getOffset(offset: lastOffset))
+            .offset(getOffset(offset: offset))
+            .simultaneousGesture(
+                DragGesture(minimumDistance: minimumDistance, coordinateSpace: .global)
+                    .onChanged({ value in
+                        onChanged?(value.translation)
+                        
+                        withAnimation(animation) {
+                            offset = value.translation
+                            
+                            rotation = getRotation(translation: value.translation)
+                            scale = getScale(translation: value.translation)
+                        }
+                    })
+                    .onEnded({ value in
+                        if !resets {
+                            onEnded?(lastOffset)
+                        } else {
+                            onEnded?(value.translation)
+                        }
+
+                        withAnimation(animation) {
+                            offset = .zero
+                            rotation = 0
+                            scale = 1
+                            
+                            if !resets {
+                                lastOffset = CGSize(
+                                    width: lastOffset.width + value.translation.width,
+                                    height: lastOffset.height + value.translation.height)
+                            } else {
+                                onChanged?(offset)
+                            }
+                        }
+                    })
+            )
+    }
+    
+    
+    private func getOffset(offset: CGSize) -> CGSize {
+        switch axes {
+        case .vertical:
+            return CGSize(width: 0, height: offset.height)
+        case .horizontal:
+            return CGSize(width: offset.width, height: 0)
+        default:
+            return offset
+        }
+    }
+    
+    private func getRotation(translation: CGSize) -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let percentage = translation.width * rotationMultiplier / max
+        let maxRotation: CGFloat = 10
+        return percentage * maxRotation
+    }
+    
+    private func getScale(translation: CGSize) -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        
+        var offsetAmount: CGFloat = 0
+        switch axes {
+        case .vertical:
+            offsetAmount = abs(translation.height + lastOffset.height)
+        case .horizontal:
+            offsetAmount = abs(translation.width + lastOffset.width)
+        default:
+            offsetAmount = (abs(translation.width + lastOffset.width) + abs(translation.height + lastOffset.height)) / 2
+        }
+        
+        let percentage = offsetAmount * scaleMultiplier / max
+        let minScale: CGFloat = 0.8
+        let range = 1 - minScale
+        return 1 - (range * percentage)
+    }
+    
+}
+
+public extension View {
+    
+    /// Add a DragGesture to a View.
+    ///
+    /// DragGesture is added as a simultaneousGesture, to not interfere with other gestures Developer may add.
+    ///
+    /// - Parameters:
+    ///   - axes: Determines the drag axes. Default allows for both horizontal and vertical movement.
+    ///   - resets: If the View should reset to starting state onEnded.
+    ///   - animation: The drag animation.
+    ///   - rotationMultiplier: Used to rotate the View while dragging. Only applies to horizontal movement.
+    ///   - scaleMultiplier: Used to scale the View while dragging.
+    ///   - onEnded: The modifier will handle the View's offset onEnded. This escaping closure is for Developer convenience.
+    ///
+    func withDragGesture(
+        _ axes: Axis.Set = [.horizontal, .vertical],
+        minimumDistance: CGFloat = 0,
+        resets: Bool = true,
+        animation: Animation = .spring(response: 0.3, dampingFraction: 0.8, blendDuration: 0.0),
+        rotationMultiplier: CGFloat = 0,
+        scaleMultiplier: CGFloat = 0,
+        onChanged: ((_ dragOffset: CGSize) -> ())? = nil,
+        onEnded: ((_ dragOffset: CGSize) -> ())? = nil) -> some View {
+            modifier(DragGestureViewModifier(axes, minimumDistance: minimumDistance, resets: resets, animation: animation, rotationMultiplier: rotationMultiplier, scaleMultiplier: scaleMultiplier, onChanged: onChanged, onEnded: onEnded))
+    }
+    
+}
+
+struct DragGestureViewModifier_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .frame(width: 300, height: 200)
+            .withDragGesture(
+                [.vertical, .horizontal],
+                resets: true,
+                animation: .smooth,
+                rotationMultiplier: 1.1,
+                scaleMultiplier: 1.1,
+                onChanged: { dragOffset in
+                    let tx = dragOffset.height
+                    let ty = dragOffset.width
+                    print(tx)
+                    print(ty)
+                },
+                onEnded: { dragOffset in
+                    let tx = dragOffset.height
+                    let ty = dragOffset.width
+                    print(tx)
+                    print(ty)
+                }
+            )
+    }
+}
